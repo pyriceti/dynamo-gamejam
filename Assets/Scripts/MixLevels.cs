acf35b970fd88b1b1fdf55f43d3c1e785af93e14
Baptiste Perraud <baptiste.perraud@orange.fr>
2018-10-17 11:48:40 +0200

    Add mixLevels script

---
+++ Assets/Scripts/MixLevels.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour {
+    public AudioMixer MasterMixer;
+
+    public void SetSfxLevel(float sfxLevel) {
+        MasterMixer.SetFloat("sfxVol", sfxLevel);
+    }
+
+    public void SetMusicLevel(float musicLevel) {
+        MasterMixer.SetFloat("musicVol", musicLevel);
+    }
+}
