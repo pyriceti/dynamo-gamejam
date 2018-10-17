50051427374356124d2d4c0154710a78afb2ecfc
Baptiste Perraud <baptiste.perraud@orange.fr>
2018-10-17 12:15:54 +0200

    Add AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance;
+
+    private void Awake() {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+}
